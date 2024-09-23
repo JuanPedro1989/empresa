4711a71a295f7dd99633a7b459c40dfb338d77a6
JuanPedro1989 <juanpedrolg1989gmail.com>
2024-09-23 13:00:02 +0200

    Segon commit

---
+++ src/models/index.ts
@@ -0,0 +1,5 @@
+export * from './client.model';
+export * from './comanda.model';
+export * from './detall-de-la-comanda.model';
+export * from './empleat.model';
+export * from './producte.model';
---
+++ src/repositories/index.ts
@@ -0,0 +1,5 @@
+export * from './client.repository';
+export * from './comanda.repository';
+export * from './detall-de-la-comanda.repository';
+export * from './empleat.repository';
+export * from './producte.repository';
